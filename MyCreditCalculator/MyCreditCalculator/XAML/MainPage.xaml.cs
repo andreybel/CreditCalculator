573004725fc0e80abc8899c67beb827ba69e0a44
andreybell123 <andreybell123@gmail.com>
2019-11-15 16:34:12 +0300

    Ready to update

--- MyCreditCalculator/MyCreditCalculator/XAML/MainPage.xaml.cs
+++ MyCreditCalculator/MyCreditCalculator/XAML/MainPage.xaml.cs
@@ -23,17 +23,17 @@ namespace MyCreditCalculator
             var item = e.SelectedItem as MasterPageItem;
             if (item != null)
             {
-                //if (item.Title.Equals("Выход"))
-                //    System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
-                //else
-                //{
+                if (item.Title.Equals("Выход"))
+                    System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+                else
+                {
                     await Task.Factory.StartNew(() =>
                     {
                         Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
                     });
                     IsPresented = false;
                     masterPage.listView.SelectedItem = null;
-                //}
+                }
                    
             }
         }
